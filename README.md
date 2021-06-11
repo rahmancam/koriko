1501501afcf214d604f40dbd8fba2fc819a20ef1
Abdul Rahman <rahmancam@live.com>
2021-06-11 15:53:53 +0530

    Add deliveryCost with baseEstimator

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 
-[![CI](https://img.shields.io/github/workflow/status/rahmancam/koriko/Koriko%20CI)](https://github.com/rahmancam/koriko)
+[![CI](https://img.shields.io/github/workflow/status/rahmancam/koriko/Koriko%20CI?label=tests)](https://github.com/rahmancam/koriko/actions)
+
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
 
 # Koriko
--- db/courier-db.js
+++ db/courier-db.js
@@ -1,4 +1,4 @@
-import InvalidConfigurationError from '../lib/errors/invalid-configuration-error'
+import validateSettingsOrThrowError from '../lib/helpers/validate-settings'
 
 export default function makeCourierDb ({ makeDb }) {
   return Object.freeze({
@@ -14,27 +14,7 @@ export default function makeCourierDb ({ makeDb }) {
 
   function findAllSettings () {
     const db = makeDb()
-
-    if (!db || !db.settings) {
-      throw new InvalidConfigurationError('DB and settings are not configured')
-    }
-
-    if (!db.settings.costPerKg) {
-      throw new InvalidConfigurationError('costPerKg is not configured in settings')
-    }
-
-    if (db.settings.costPerKg <= 0) {
-      throw new InvalidConfigurationError('costPerKg should not be zero or negative')
-    }
-
-    if (!db.settings.costPerKm) {
-      throw new InvalidConfigurationError('costPerKm is not configured in settings')
-    }
-
-    if (db.settings.costPerKm <= 0) {
-      throw new InvalidConfigurationError('costPerKm should be zero or negative')
-    }
-
+    validateSettingsOrThrowError(db)
     return db.settings
   }
 }
