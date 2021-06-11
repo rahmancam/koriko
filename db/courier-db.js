abc9f38f1b11d6dcfcd9bec50e6806a9cfe0ccd0
Abdul Rahman <rahmancam@live.com>
2021-06-11 12:24:46 +0530

    Add settings for costPerKg and costPerkm

--- db/courier-db.js
+++ db/courier-db.js
@@ -1,10 +1,40 @@
+import InvalidConfigurationError from '../lib/models/errors/invalid-configuration-error'
+
 export default function makeCourierDb ({ makeDb }) {
   return Object.freeze({
-    findAllCoupons
+    findAllCoupons,
+    findAllSettings
   })
 
   function findAllCoupons () {
     const db = makeDb()
+    // coupons are optional
     return db?.coupons
+  }
+
+  function findAllSettings () {
+    const db = makeDb()
+
+    if (!db || !db.settings) {
+      throw new InvalidConfigurationError('DB and settings are not configured')
+    }
+
+    if (!db.settings.costPerKg) {
+      throw new InvalidConfigurationError('costPerKg is not configured in settings')
+    }
+
+    if (db.settings.costPerKg <= 0) {
+      throw new InvalidConfigurationError('costPerKg should not be zero or negative')
+    }
+
+    if (!db.settings.costPerKm) {
+      throw new InvalidConfigurationError('costPerKm is not configured in settings')
+    }
+
+    if (db.settings.costPerKm <= 0) {
+      throw new InvalidConfigurationError('costPerKm should be zero or negative')
+    }
+
+    return db.settings
   }
 }
--- db/courier-db.spec.js
+++ db/courier-db.spec.js
@@ -1,3 +1,4 @@
+import InvalidConfigurationError from '../lib/models/errors/invalid-configuration-error'
 import makeCourierDb from './courier-db'
 
 describe('Courier DB', () => {
@@ -19,5 +20,63 @@ describe('Courier DB', () => {
     const courierDB = makeCourierDb({ makeDb })
     const allCoupons = courierDB.findAllCoupons()
     expect(makeDb().coupons).toEqual(allCoupons)
+  })
+
+  test('Should throw error if no settings configured', () => {
+    const makeDb = () => ({})
+    const courierDB = makeCourierDb({ makeDb })
+    expect(() => {
+      courierDB.findAllSettings()
+    }).toThrow(InvalidConfigurationError)
+  })
+
+  test('Should throw error if costPerKg configured is zero', () => {
+    const makeDb = function () {
+      return { settings: { costPerKg: 0, costPerKm: 10 } }
+    }
+    const courierDB = makeCourierDb({ makeDb })
+    expect(() => {
+      courierDB.findAllSettings()
+    }).toThrow(InvalidConfigurationError)
+  })
+
+  test('Should throw error if costPerKg configured is negative', () => {
+    const makeDb = function () {
+      return { settings: { costPerKg: -10, costPerKm: 10 } }
+    }
+    const courierDB = makeCourierDb({ makeDb })
+    expect(() => {
+      courierDB.findAllSettings()
+    }).toThrow(InvalidConfigurationError)
+  })
+
+  test('Should throw error if costPerKm configured is zero', () => {
+    const makeDb = function () {
+      return { settings: { costPerKg: 10, costPerKm: 0 } }
+    }
+    const courierDB = makeCourierDb({ makeDb })
+    expect(() => {
+      courierDB.findAllSettings()
+    }).toThrow(InvalidConfigurationError)
+  })
+
+  test('Should throw error if costPerKm configured is negative', () => {
+    const makeDb = function () {
+      return { settings: { costPerKg: 10, costPerKm: -5 } }
+    }
+    const courierDB = makeCourierDb({ makeDb })
+    expect(() => {
+      courierDB.findAllSettings()
+    }).toThrow(InvalidConfigurationError)
+  })
+
+  test('Should get valid settings if configured', () => {
+    const makeDb = function () {
+      return { settings: { costPerKg: 10, costPerKm: 5 } }
+    }
+    const courierDB = makeCourierDb({ makeDb })
+    expect(
+      courierDB.findAllSettings()
+    ).toEqual(makeDb().settings)
   })
 })
